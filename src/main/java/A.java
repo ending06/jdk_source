92a6f317126afe3db2160971a59cb23fddcd36c0
ruirui.qu <ruirui.qu@qunar.com>
2019-05-15 14:18:07 +0800

    init

---
+++ src/main/java/A.java
@@ -0,0 +1,10 @@
+//--------------------- Change Logs----------------------
+// <p>@author ruirui.qu Initial Created at 2019/5/15<p>
+//-------------------------------------------------------
+
+public class A {
+
+    public static void main(String[] args) {
+        System.out.println("===>");
+    }
+}
